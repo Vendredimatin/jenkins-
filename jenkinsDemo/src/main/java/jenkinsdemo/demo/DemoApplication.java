ec906e73cf7a4876a47d3f2caaef9820a9b44e84
Liu Hanyi <161250072@smail.nju.edu.cn>
2019-05-24 03:06:09 +0800

    m

--- jenkinsDemo/src/main/java/jenkinsdemo/demo/DemoApplication.java
+++ jenkinsDemo/src/main/java/jenkinsdemo/demo/DemoApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 @RestController
 @SpringBootApplication
-@RequestMapping("/home")
+@RequestMapping("/admin")
 public class DemoApplication {
-    @RequestMapping("/admin")
+    @RequestMapping("/home")
     String home(HttpServletRequest request) {
         String userName = "Jef";
         int count = 101;
